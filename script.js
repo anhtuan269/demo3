3f6506624aef47fa28b62d13a722a92491648ef1
zeroo <anhtuan090795@gmail.com>
2020-10-01 01:04:29 +0700

    demo-3

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    const owl = $('.product-show');
+    owl.owlCarousel({
+        nav: true,
+        loop: true,
+        autoplay: true,
+        responsive: {
+            1000: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            0: {
+                items: 1
+            }
+        }
+    });
+    owl.on('changed.owl.carousel',function(e){
+        var index = e.page.index;
+            for (i = 0; i <= 3; i++) {
+                $('#dot' + i).removeClass("border")
+            }
+            $('#dot' + index).addClass("border");
+    } )
+});
